f4353171e0e9229b1cf41e2d4ac3aab2f4fcd48a
Mlie <mlie@litet.net>
2021-12-28 19:59:48 +0100

    Updated VersionFromManifest-file to not get the 'loading is already in progress' error with some mods

--- Source/FoodAlert/FoodAlertMod.cs
+++ Source/FoodAlert/FoodAlertMod.cs
@@ -4,58 +4,57 @@ using SettingsHelper;
 using UnityEngine;
 using Verse;
 
-namespace FoodAlert
+namespace FoodAlert;
+
+internal class FoodAlertMod : Mod
 {
-    internal class FoodAlertMod : Mod
+    public static FoodAlertSettings settings;
+
+    private static string currentVersion;
+
+    private static readonly string[] preferabilities =
+        { "DesperateOnly", "RawBad", "RawTasty", "MealAwful", "MealSimple", "MealFine", "MealLavish" };
+
+    public FoodAlertMod(ModContentPack content) : base(content)
     {
-        public static FoodAlertSettings settings;
+        settings = GetSettings<FoodAlertSettings>();
+        currentVersion =
+            VersionFromManifest.GetVersionFromModMetaData(ModLister.GetActiveModWithIdentifier("Mlie.FoodAlert"));
+    }
 
-        private static string currentVersion;
+    public override string SettingsCategory()
+    {
+        return "Food Alert";
+    }
 
-        private static readonly string[] preferabilities =
-            { "DesperateOnly", "RawBad", "RawTasty", "MealAwful", "MealSimple", "MealFine", "MealLavish" };
-
-        public FoodAlertMod(ModContentPack content) : base(content)
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        var listing_Standard = new Listing_Standard();
+        listing_Standard.Begin(inRect);
+        listing_Standard.AddLabeledRadioList("SettingDescription".Translate(), preferabilities,
+            ref settings.foodPreferability);
+        listing_Standard.Label("SettingExplanation".Translate());
+        listing_Standard.GapLine();
+        listing_Standard.Label("FA.updatetype.label".Translate());
+        listing_Standard.CheckboxLabeled("FA.typedynamic.label".Translate(), ref settings.dynamicupdate,
+            "FA.typedynamic.description".Translate());
+        if (!settings.dynamicupdate)
         {
-            settings = GetSettings<FoodAlertSettings>();
-            currentVersion =
-                VersionFromManifest.GetVersionFromModMetaData(ModLister.GetActiveModWithIdentifier("Mlie.FoodAlert"));
+            listing_Standard.AddLabeledSlider(
+                "FA.typestatic.slider".Translate(Math.Round((decimal)settings.updatefrequency / 2500, 2)),
+                ref settings.updatefrequency, 100, 10000);
         }
 
-        public override string SettingsCategory()
+        if (currentVersion != null)
         {
-            return "Food Alert";
+            listing_Standard.Gap();
+            GUI.contentColor = Color.gray;
+            listing_Standard.Label("FA.modversion".Translate(currentVersion));
+            GUI.contentColor = Color.white;
         }
 
-        public override void DoSettingsWindowContents(Rect inRect)
-        {
-            var listing_Standard = new Listing_Standard();
-            listing_Standard.Begin(inRect);
-            listing_Standard.AddLabeledRadioList("SettingDescription".Translate(), preferabilities,
-                ref settings.foodPreferability);
-            listing_Standard.Label("SettingExplanation".Translate());
-            listing_Standard.GapLine();
-            listing_Standard.Label("FA.updatetype.label".Translate());
-            listing_Standard.CheckboxLabeled("FA.typedynamic.label".Translate(), ref settings.dynamicupdate,
-                "FA.typedynamic.description".Translate());
-            if (!settings.dynamicupdate)
-            {
-                listing_Standard.AddLabeledSlider(
-                    "FA.typestatic.slider".Translate(Math.Round((decimal)settings.updatefrequency / 2500, 2)),
-                    ref settings.updatefrequency, 100, 10000);
-            }
+        listing_Standard.End();
 
-            if (currentVersion != null)
-            {
-                listing_Standard.Gap();
-                GUI.contentColor = Color.gray;
-                listing_Standard.Label("FA.modversion".Translate(currentVersion));
-                GUI.contentColor = Color.white;
-            }
-
-            listing_Standard.End();
-
-            settings.Write();
-        }
+        settings.Write();
     }
 }
